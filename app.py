5bbc05bcd2cbf5625d45cce2c88a6b2e9a3e6c5a
artemijsem <artsem0107@gmail.com>
2024-11-20 14:25:52 +0200

    cors introduction

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
 
 # Set the correct password (in a real app, store this securely, e.g., hashed in a database)
 CORRECT_PASSWORD = "secure123"
-
+CORS(app)
 
 @app.route('/check-password', methods=['POST'])
 def check_password():
@@ -20,5 +21,5 @@ def check_password():
 
 
 if __name__ == '__main__':
-    app.run(host='passcheckflask-production.up.railway.app', port=5000)
+    app.run(host='https://passcheckflask-production.up.railway.app', port=5000)
 
